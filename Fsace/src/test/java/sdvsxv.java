e15cb3c0fa278e2357d85a2df682057cfa714b34
Sujit Ganeash S <sujitganeash@riota.in>
2026-01-06 11:32:32 +0530

    New Folders merged

---
+++ Fsace/src/test/java/sdvsxv.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class sdvsxv {
+    public static void main(String[] args) throws InterruptedException {
+       
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        Thread.sleep(20000);
+        driver.quit();
+    }
+}
